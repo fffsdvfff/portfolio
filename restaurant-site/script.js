454f1f4576989d7810be7c569cf9c1d22429f355
Ella Sivaeva <145915722+fffsdvfff@users.noreply.github.com>
2026-07-06 23:46:39 +0300

    Create script.js

---
+++ restaurant-site/script.js
@@ -0,0 +1,17 @@
+const form = document.querySelector(".booking-form");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  alert("Дякуємо! Ми зв’яжемося з вами для підтвердження бронювання.");
+  form.reset();
+});
+
+const form = document.querySelector(".booking-form");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  alert("Дякуємо! Ми зв’яжемося з вами для підтвердження бронювання.");
+
+  form.reset();
+});
